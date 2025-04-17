f0203de1dbd60f401c9ca0e11307053521640164
maxoliinyk <ooomaxxx@icloud.com>
2025-04-17 13:48:53 +0300

    Initial Commit

---
+++ Translator–TestProj/Translator_TestProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  Translator_TestProjApp.swift
+//  Translator–TestProj
+//
+//  Created by Max Oliinyk on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Translator_TestProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
